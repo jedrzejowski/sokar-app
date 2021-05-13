9567ccfee4f40931fca89a7e09fc73613c7a9b8d
Adam Jędrzejowski <adam@jedrzejowski.pl>
2021-05-14 00:50:38 +0200

    dorbne poprawki

--- gdcm.mk
+++ gdcm.mk
@@ -1,25 +1,28 @@
 
 GDCM_VERSION=3.0.8
 
-.PHONY: all download extract cmake build
+.DEFAULT_GOAL:=build
 
-all: download extract cmake build
-
-download:
+lib/gdcm-${GDCM_VERSION}.tar.gz:
 	mkdir -p lib/
 	wget -O lib/gdcm-${GDCM_VERSION}.tar.gz \
 		https://sourceforge.net/projects/gdcm/files/gdcm%203.x/GDCM%20${GDCM_VERSION}/gdcm-${GDCM_VERSION}.tar.gz/download
 
-extract:
+lib/gdcm-${GDCM_VERSION}:
 	rm -rf lib/gdcm-${GDCM_VERSION}
 	mkdir -p lib/gdcm-${GDCM_VERSION}
 	tar -xf lib/gdcm-${GDCM_VERSION}.tar.gz -C lib/
 
-cmake:
+lib/gdcm-${GDCM_VERSION}/Makefile:
 	rm -rf lib/gdcm-bin
 	mkdir -p lib/gdcm-bin
 
 	cmake -S lib/gdcm-${GDCM_VERSION} -B lib/gdcm-bin
 
-build:
+lib/gdcm-bin/*.a:
 	cd lib/gdcm-bin && make
+
+download: lib/gdcm-${GDCM_VERSION}.tar.gz
+extract: download lib/gdcm-${GDCM_VERSION}
+cmake: extract lib/gdcm-${GDCM_VERSION}/Makefile
+build: cmake lib/gdcm-bin/*.a
